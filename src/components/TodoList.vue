16548317a2cffd1fa5107f4f9e8531f97dbd025d
Billie Zhang <billiecoder@gmail.com>
2018-05-08 14:37:10 +0800

    primary

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="wrap">
     List
+    <div class="primary">
+      Primary
+    </div>
+    <div class="accent">
+      Accent
+    </div>
   </div>
 </template>
 
@@ -10,8 +16,12 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '@/styles/variables.scss';
 .wrap {
   border: 2px solid lightblue;
+  .primary {
+    color: $primary-color;
+  }
 }
 </style>
---
+++ src/constants/Palette.js
@@ -0,0 +1,4 @@
+export const PRIMARY_PINK = '#F8D5F0' // PRIMARY 主要
+export const ACCENT_BLUE = '#41A4C3' // ACCENT 强调
+export const ACCENT_LIGHT = '#58D5D3' // 浅
+export const ACCENT_DARK = '#3F4B83' // 深强调色
